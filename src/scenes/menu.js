a4de85f7f7da9e5fcb842dc0aa4d387c35bf3b29
mimehuan <mimehuan@gmail.com>
2022-04-14 18:30:14 -0700

    changed asset names in menu

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -5,9 +5,9 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('sfx_select', './assets/assets_blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/assets_explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/assets_rocket_shot.wav');
     }
 
     create() {
